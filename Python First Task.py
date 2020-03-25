74b8b9a613e5a4ac119129e7609d2c92d0d6b825
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2020-03-25 23:50:08 +0100

    Python First Task

---
+++ Python First Task.py
@@ -0,0 +1,7 @@
+#A Python program which accepts the radius of a circle from the user and computes the area.
+print("SlackUsername- BlackAdam")
+print("Hello User")
+r = float(input ("Please Enter radius of the circle : "))
+pi = 3.142
+print ("The area of the circle with radius " + str(r) + " is:- " + str(pi * r**2))
+print("Thank you")
